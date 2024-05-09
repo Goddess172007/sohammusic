d65f977a1b951132a6240c5aef7de8c25db1cff3
soham6540 <savitafarkade6540@gmail.com>
2024-05-09 14:38:58 +0530

    Add files via upload

---
+++ GOKUMUSIC/__init__.py
@@ -0,0 +1,26 @@
+from GOKUMUSIC.core.bot import GOKU
+from GOKUMUSIC.core.dir import dirr
+from GOKUMUSIC.core.userbot import Userbot
+from GOKUMUSIC.music import dbb, heroku
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+dbb()
+heroku()
+
+app = GOKU()
+api = SafoneAPI()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
