88f00e7a1a4aee29a9e93462e3112e4cd325daf6
Goddess172007 <bak01072007@gmail.com>
2024-10-17 04:34:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return send_from_directory('shivu', 'index.html')
+    return send_from_directory('GOKUMUSIC', 'index.html')
 
 
 if __name__ == '__main__':
